dc920f36cc6a079a22bf787f08d98e1bd36a1435
Alex Norton <alex@alexnorton.com>
2019-02-19 19:10:32 +0000

    Make header brand link to app root

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 
 import SearchPage from './pages/search-page';
@@ -7,21 +7,23 @@ import RestaurantPage from './pages/restaurant-page';
 
 function App() {
   return (
-    <>
-      <Navbar color="light" light>
-        <Container>
-          <NavbarBrand>New York Restaurants</NavbarBrand>
-        </Container>
-      </Navbar>
-      <Container className="mt-4">
-        <Router>
+    <Router>
+      <div>
+        <Navbar color="dark" dark>
+          <Container>
+            <NavbarBrand tag={Link} to="/">
+              New York Restaurants
+            </NavbarBrand>
+          </Container>
+        </Navbar>
+        <Container className="mt-4">
           <div>
             <Route exact path="/" component={SearchPage} />
             <Route path="/:camis" component={RestaurantPage} />
           </div>
-        </Router>
-      </Container>
-    </>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
