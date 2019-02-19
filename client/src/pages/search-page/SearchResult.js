5dc3a672d0dfc00b32b3e3c41814f7c4541a5329
Alex Norton <alex@alexnorton.com>
2019-02-19 19:04:55 +0000

    Visualise grade using badge in search results

--- client/src/pages/search-page/SearchResult.js
+++ client/src/pages/search-page/SearchResult.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const LETTER_GRADE_COLOURS = {
+  A: 'success',
+  B: 'warning',
+  C: 'danger',
+};
+
 function SearchResult({
   result: {
     camis,
@@ -13,29 +19,35 @@ function SearchResult({
     phone,
     lastInspectionDate,
     lastInspectionGrade,
-    lastInspectionScore,
   },
 }) {
+  const gradeColour = LETTER_GRADE_COLOURS[lastInspectionGrade] || 'secondary';
+
   return (
-    <div>
-      <Link to={`/${camis}`}>
-        <h3>{name}</h3>
-      </Link>
-      <p>
-        <strong>Cuisine:</strong> {cuisine}
-        <br />
-        <strong>Address:</strong> {building} {street}, {borough}, {zipCode}
-        <br />
-        <strong>Phone:</strong> {phone}
-      </p>
-      <h4>Last inspection</h4>
-      <p>
-        <strong>Date:</strong> {lastInspectionDate}
-        <br />
-        <strong>Grade:</strong> {lastInspectionGrade}
-        <br />
-        <strong>Score:</strong> {lastInspectionScore}
-      </p>
+    <div className="d-flex align-items-center mb-4">
+      <div className="flex-grow-1">
+        <Link to={`/${camis}`}>
+          <h3>{name}</h3>
+        </Link>
+        <p className="mb-0">
+          <strong>Cuisine:</strong> {cuisine}
+          <br />
+          <strong>Address:</strong> {building} {street}, {borough}, {zipCode}
+          <br />
+          <strong>Phone:</strong> {phone}
+        </p>
+      </div>
+      <div
+        className={`bg-${gradeColour} rounded-circle text-center text-white d-flex flex-column justify-content-center`}
+        style={{
+          width: 90,
+          height: 90,
+          fontSize: LETTER_GRADE_COLOURS[lastInspectionGrade] && 40,
+        }}
+        title={`Last inspected on ${lastInspectionDate}`}
+      >
+        {lastInspectionGrade}
+      </div>
     </div>
   );
 }
