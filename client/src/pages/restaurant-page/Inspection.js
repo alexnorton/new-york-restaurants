11573cb5057620f813ef1f71f52df53f00449d7f
Alex Norton <alexnorton@fastmail.com>
2019-02-21 17:14:44 +0000

    Don't use inline styles where avoidable

--- client/src/pages/restaurant-page/Inspection.js
+++ client/src/pages/restaurant-page/Inspection.js
@@ -9,39 +9,41 @@ function Inspection({ startOpen, inspection }) {
     <Card className="mb-2">
       <CardHeader
         onClick={() => setOpen(!open)}
-        style={{ cursor: 'pointer', borderBottom: !open && 'none' }}
-        className="d-flex align-items-center"
+        className={`d-flex align-items-center cursor-pointer ${!open &&
+          'border-bottom-0'}`}
       >
         <h4 className="flex-grow-1 mb-0">
           {new Date(inspection.date).toLocaleDateString()}
         </h4>
         <GradeBadge grade={inspection.grade} size={45} fontSize={20} />
       </CardHeader>
-      <CardBody style={{ display: !open && 'none' }}>
-        <p className="mb-0">
-          <strong>Type:</strong> {inspection.type}
-          <br />
-          <strong>Grade:</strong> {inspection.grade}
-          <br />
-          <strong>Score:</strong> {inspection.score}
-          <br />
-          <strong>Action:</strong> {inspection.action}
-        </p>
-        {inspection.violations.length > 0 && (
-          <>
-            <h5 className="mt-3">Violations</h5>
-            <ul className="mb-0">
-              {inspection.violations.map(
-                ({ violationId, code, description, criticalFlag }) => (
-                  <li key={violationId}>
-                    {code} - {description} ({criticalFlag})
-                  </li>
-                )
-              )}
-            </ul>
-          </>
-        )}
-      </CardBody>
+      {open && (
+        <CardBody>
+          <p className="mb-0">
+            <strong>Type:</strong> {inspection.type}
+            <br />
+            <strong>Grade:</strong> {inspection.grade}
+            <br />
+            <strong>Score:</strong> {inspection.score}
+            <br />
+            <strong>Action:</strong> {inspection.action}
+          </p>
+          {inspection.violations.length > 0 && (
+            <>
+              <h5 className="mt-3">Violations</h5>
+              <ul className="mb-0">
+                {inspection.violations.map(
+                  ({ violationId, code, description, criticalFlag }) => (
+                    <li key={violationId}>
+                      {code} - {description} ({criticalFlag})
+                    </li>
+                  )
+                )}
+              </ul>
+            </>
+          )}
+        </CardBody>
+      )}
     </Card>
   );
 }
